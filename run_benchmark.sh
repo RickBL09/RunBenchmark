a48e7509896af5dcc2f4ac26e52cd0491361adad
Ricardo Javier Brito López <115487157+RickBL09@users.noreply.github.com>
2025-02-14 12:28:43 -0500

    Update run_benchmark.sh

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,9 +1,11 @@
-
 echo "====================================="
 echo "    EJECUTANDO BENCHMARK"
 echo "====================================="
 
-languages=("java" "javascript" "python" "c" "csharp")
+languages=("java" "javascript" "python" "c" "go")
+
+# Imprimir cabecera de la tabla
+echo -e "Lenguaje\t\tTiempo de ejecución (ms)"
 
 for lang in "${languages[@]}"; do
     echo ""
@@ -11,12 +13,18 @@ for lang in "${languages[@]}"; do
     
     cd /app/codes/$lang
     
-    docker build -t benchmark-$lang .
+    # Construir la imagen sin mostrar la salida en la terminal
+    docker build -t benchmark-$lang . > /dev/null 2>&1
     echo "Contenedor $lang construido."
 
-    time=$(docker run --rm benchmark-$lang)
+    # Usar "time" para medir el tiempo y extraer el tiempo de ejecución en milisegundos
+    execution_time=$( { time docker run --rm benchmark-$lang ;} 2>&1 | grep real | awk '{print $2}' | sed 's/m/*60+/g' | bc)
+
+    # Mostrar el tiempo en milisegundos (multiplicando por 1000 si es necesario)
+    execution_time_ms=$(echo "$execution_time * 1000" | bc)
     
-    echo "Tiempo de ejecución en $lang: ${time} ms"
+    # Imprimir el resultado en la tabla
+    echo -e "$lang\t\t$execution_time_ms ms"
     
     cd /app/codes
 done
