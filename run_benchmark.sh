924d8433c7ae193d815d07a4ffd2152232c47f86
Ricardo Javier Brito López <115487157+RickBL09@users.noreply.github.com>
2025-02-13 21:20:49 -0500

    Update run_benchmark.sh

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,7 +3,7 @@ echo "====================================="
 echo "    EJECUTANDO BENCHMARK"
 echo "====================================="
 
-languages=("openjdk" "node" "python" "gcc" "dotnet")
+languages=("java" "javascript" "python" "c" "csharp")
 
 for lang in "${languages[@]}"; do
     echo ""
